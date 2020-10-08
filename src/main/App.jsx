3e2fd803feb448acec57a02e4b54ee55f68515ad
Rodrigo Junior <rodrigo.junior7@outlook.com>
2020-10-08 20:38:32 -0300

    first commit

---
+++ src/main/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React from "react";
+
+import Menu from "../template/Menu";
+import Routes from './routes';
+
+export default props => {
+  return (
+    <div className="container">
+      <Menu />
+      <Routes />
+    </div>
+  );
+}
